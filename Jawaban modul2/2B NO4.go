6ed9b0b141b43215d82ff80843ee1a37ca040ef0
Kazxcs <155834336+Kazxcs@users.noreply.github.com>
2024-10-11 00:45:53 +0700

    Update 2B NO4.go

--- Jawaban modul2/2B NO4.go
+++ Jawaban modul2/2B NO4.go
@@ -3,46 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	var beratKantong1, beratKantong2 float64
+	var k int
+	var hasil float64
+	fmt.Print("Nilai K: ")
+	fmt.Scan(&k)
 
-	for {
-		fmt.Print("Masukan berat belanjaan di kantong pertama: ")
-		_, err1 := fmt.Scan(&beratKantong1)
-		fmt.Print("Masukan berat belanjaan di kantong kedua: ")
-		_, err2 := fmt.Scan(&beratKantong2)
+	pembilang := float64((4*k + 2) * (4*k + 2))
+	penyebut := float64((4*k + 1) * (4*k + 3))
+	hasil = pembilang / penyebut
 
-		// Cek jika input tidak valid
-		if err1 != nil || err2 != nil {
-			fmt.Println("Input tidak valid. Harap masukkan angka.")
-			continue
-		}
-
-		// Cek jika salah satu berat negatif
-		if beratKantong1 < 0 || beratKantong2 < 0 {
-			fmt.Println("Proses selesai.")
-			break
-		}
-
-		// Hitung total berat
-		totalBerat := beratKantong1 + beratKantong2
-
-		// Cek jika total berat lebih dari 150 kg
-		if totalBerat > 150 {
-			fmt.Println("Proses selesai.")
-			break
-		}
-
-		// Hitung selisih berat
-		selisihBerat := beratKantong1 - beratKantong2
-		if selisihBerat < 0 {
-			selisihBerat = -selisihBerat
-		}
-
-		// Tampilkan apakah sepeda motor oleng atau tidak
-		if selisihBerat >= 9 {
-			fmt.Println("Sepeda motor pak Andi akan oleng: true")
-		} else {
-			fmt.Println("Sepeda motor pak Andi akan oleng: false")
-		}
-	}
+	fmt.Printf("Nilai f(K) = %.10f\n", hasil)
 }
